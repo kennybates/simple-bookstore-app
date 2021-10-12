3878ef95d57505bfde8a229fc47f9b8794ffc4f9
Kenny Bates <kennyleebates@outlook.com>
2021-10-12 18:11:14 -0400

    index.js update functions for books
    Create a working template/layout to begin the booklist and books structure.

--- src/index.js
+++ src/index.js
@@ -2,17 +2,33 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 
-
+// Function Booklist - holds instances of each Book 
 function Booklist() {
   return (
     <section> 
+     <Book />
+     <Book />
      <Book />
     </section>
   )
 };
 
+// Book arrow function
 const Book = () => {
-  return <article> This is a book.</article>
+  return (
+    <article>
+      <Image></Image>
+      <Title />
+      <Author />
+    </article>
+  )
 };
+
+// Book components Image, Title, Author
+const Image = () => <img src="https://m.media-amazon.com/images/I/71gk+H+yEkL._AC_UY218_.jpg" alt="" />
+
+const Title = () => <h1>Peril</h1>;
+
+const Author = () => <h2>Bob Woodward</h2>;
 
 ReactDom.render(<Booklist />, document.getElementById('root'));
