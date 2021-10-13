f9867ca5e86ded893ff8c02d64a3d1f1342f54b7
Kenny Bates <kennyleebates@outlook.com>
2021-10-13 16:54:08 -0400

    Created book.js
    created books.js to put the book data array in to help clean up code.

---
+++ src/books.js
@@ -0,0 +1,39 @@
+// A list created to store book values to iterate over 
+export const data = [
+    {
+      id: 1,
+      img: 'https://m.media-amazon.com/images/I/71gk+H+yEkL._AC_UY218_.jpg',
+      title: 'Peril',
+      author: 'Bob Woodward',
+    },
+    {
+      id: 2,
+      img: 'https://images-na.ssl-images-amazon.com/images/I/91tjTEpgh6L._AC_UL200_SR200,200_.jpg',
+      title: 'Silverview: A Novel',
+      author: 'John le carre',
+    },
+    {
+      id: 3,
+      img: 'https://images-na.ssl-images-amazon.com/images/I/81kosCB1luL._AC_UL200_SR200,200_.jpg',
+      title: 'The Storyteller',
+      author: 'Dave Grohl',
+    },
+    {
+      id: 4,
+      img: 'https://images-na.ssl-images-amazon.com/images/I/91oR39L3K8S._AC_UL200_SR200,200_.jpg',
+      title: 'The Boys',
+      author: 'Ron Howard',
+    },
+    {
+      id: 5,
+      img: 'https://images-na.ssl-images-amazon.com/images/I/71bROORU%2BnL._AC_UL200_SR200,200_.jpg',
+      title: 'The Christmas Pig',
+      author: 'J. K. Rowling',
+    },
+    {
+      id: 6,
+      img: 'https://images-na.ssl-images-amazon.com/images/I/81iAADNy2NL._AC_UL200_SR200,200_.jpg',
+      title: 'Atomic Habits',
+      author: 'James Clear',
+    },
+  ];
--- src/index.js
+++ src/index.js
@@ -1,28 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css'; 
-
-// A list created to store book values to iterate over 
-const books = [
-  {
-    id: 1,
-    img: 'https://m.media-amazon.com/images/I/71gk+H+yEkL._AC_UY218_.jpg',
-    title: 'Peril',
-    author: 'Bob Woodward',
-  },
-  {
-    id: 2,
-    img: 'https://images-na.ssl-images-amazon.com/images/I/91tjTEpgh6L._AC_UL200_SR200,200_.jpg',
-    title: 'Silverview: A Novel',
-    author: 'John le carre',
-  },
-  {
-    id: 3,
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81kosCB1luL._AC_UL200_SR200,200_.jpg',
-    title: 'The Storyteller',
-    author: 'Dave Grohl',
-  },
-];
+import {data} from './books'; //import data array from books.js
 
 
 
@@ -30,23 +9,26 @@ const books = [
 function Booklist() {
   return (
     <section className='booklist'> 
-      {books.map((book) => { // iterates over each book property in list called books
-          return <Book key={book.id} book={book}></Book>;
+      {data.map((book, index) => { // iterates over each book property in list called books
+          return <Book key={book.id} {...book}></Book>;
       })}  
     </section>
   )
 };
 
 // Book function
-const Book = (props) => {
-  const { img, title, author } = props.book; // destructuring the book property
+const Book = ({ img, title, author}) => {
+  //const { img, title, author } = props; // destructuring book objects
+  const clickHandler = () => {
+    alert('Description of book');
+  };
   return (
     <article className="book">
-      <img 
-        src={img} alt="" 
-      />
+      <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>Description</button>
+      
     </article>
   )
 };
