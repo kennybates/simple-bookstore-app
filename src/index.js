c1b6c225e2f2105f1caaac9f534e15e47a45daba
Kenny Bates <kennyleebates@outlook.com>
2021-10-12 20:10:47 -0400

    Create a list for book data
    Updated index.js to include a list that contains book information with id, img, title, and author properties. Then map through these values in the Booklist function.

--- src/index.js
+++ src/index.js
@@ -2,29 +2,51 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css'; 
 
+// A list created to store book values to iterate over 
+const books = [
+  {
+    id: 1,
+    img: 'https://m.media-amazon.com/images/I/71gk+H+yEkL._AC_UY218_.jpg',
+    title: 'Peril',
+    author: 'Bob Woodward',
+  },
+  {
+    id: 2,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91tjTEpgh6L._AC_UL200_SR200,200_.jpg',
+    title: 'Silverview: A Novel',
+    author: 'John le carre',
+  },
+  {
+    id: 3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81kosCB1luL._AC_UL200_SR200,200_.jpg',
+    title: 'The Storyteller',
+    author: 'Dave Grohl',
+  },
+];
+
+
 
 // Function Booklist - holds instances of each Book 
 function Booklist() {
   return (
     <section className='booklist'> 
-     <Book />
-     <Book />
-     <Book />
-     <Book />
-     <Book />
+      {books.map((book) => { // iterates over each book property in list called books
+          return <Book key={book.id} book={book}></Book>;
+      })}  
     </section>
   )
 };
 
-// Book arrow function
-const Book = () => {
+// Book function
+const Book = (props) => {
+  const { img, title, author } = props.book; // destructuring the book property
   return (
     <article className="book">
       <img 
-        src="https://m.media-amazon.com/images/I/71gk+H+yEkL._AC_UY218_.jpg" alt="Peril Book" 
+        src={img} alt="" 
       />
-      <h1>Peril</h1>
-      <h4>Bob Woodward</h4>
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   )
 };
